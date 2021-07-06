a77688ceb186e8cff2c71c9b23b083f40b9ea7c4
jacquelineth <jacquelineth@gmail.com>
2021-07-06 15:23:19 +0000

    Install all required packages

--- datareader.py
+++ datareader.py
@@ -1,2 +1,6 @@
 import pip
 pip.main(['install', 'pandas_datareader'])
+pip.main(['install', 'pandas'])
+pip.main(['install', 'matplotlib.pyplot'])
+pip.main(['install', 'mplfinance'])
+pip.main(['install', 'seaborn'])
