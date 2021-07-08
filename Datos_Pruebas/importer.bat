be3fc4f0b36850c3352fa98afa4a3ca79808dcbf
jacquelineth <jacquelineth@gmail.com>
2021-07-08 20:30:11 +0200

    Datos de pruebas 30 dias

---
+++ Datos_Pruebas/importer.bat
@@ -0,0 +1,18 @@
+
+curl "https://query1.finance.yahoo.com/v7/finance/download/ADA-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=true" -o 
+curl "https://query1.finance.yahoo.com/v7/finance/download/BCH-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=true" -o BCH-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/BNB-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=true" -o BNB-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/BTC-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=true" -o BTC-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/DOGE-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=true" -o DOGE-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/DOT1-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=true" -o DOT1-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/ETH-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false" -o ETH-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/GC=F?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false" -o GCF_USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/ICP1-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false" -o ICP1-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/LINK-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false" -o LINK-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/LTC-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false" -o LTC-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/MATIC-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false" -o MATIC-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/SOL1-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false" -o SOL1-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/UNI3-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false" -o UNI3-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/USDC-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false" -o USDC-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/USDT-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false" -o USDT-USD.csv
+curl "https://query1.finance.yahoo.com/v7/finance/download/XRP-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false" -o XRP-USD.csv
---
+++ Datos_Pruebas/importer.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+curl https://query1.finance.yahoo.com/v7/finance/download/ADA-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=true
+curl https://query1.finance.yahoo.com/v7/finance/download/BCH-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=true
+curl https://query1.finance.yahoo.com/v7/finance/download/BNB-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=true
+curl https://query1.finance.yahoo.com/v7/finance/download/BTC-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=true
+curl https://query1.finance.yahoo.com/v7/finance/download/DOGE-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=true
+curl https://query1.finance.yahoo.com/v7/finance/download/DOT1-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=true
+curl https://query1.finance.yahoo.com/v7/finance/download/ETH-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false
+curl https://query1.finance.yahoo.com/v7/finance/download/GC=F?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false
+curl https://query1.finance.yahoo.com/v7/finance/download/ICP1-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false
+curl https://query1.finance.yahoo.com/v7/finance/download/LINK-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false
+curl https://query1.finance.yahoo.com/v7/finance/download/LTC-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false
+curl https://query1.finance.yahoo.com/v7/finance/download/MATIC-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false
+curl https://query1.finance.yahoo.com/v7/finance/download/SOL1-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false
+curl https://query1.finance.yahoo.com/v7/finance/download/UNI3-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false
+curl https://query1.finance.yahoo.com/v7/finance/download/USDC-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false
+curl https://query1.finance.yahoo.com/v7/finance/download/USDT-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false
+curl https://query1.finance.yahoo.com/v7/finance/download/XRP-USD?period1=1623110400&period2=1625702400&interval=1d&events=history&includeAdjustedClose=false
