feaf6a50bb9fd6bd12d68b2d19990c1ec8cce3e9
jacquelineth <jacquelineth@gmail.com>
2021-07-10 23:43:23 +0200

    typo error

--- CryptoTrainingModel.py
+++ CryptoTrainingModel.py
@@ -122,7 +122,7 @@ class TFMmodeler:
         x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
         predicted_prices = self.model.predict(x_test)
-        npredicted_prices = self.scaler.inverse_transform(predicted_prices)
+        predicted_prices = self.scaler.inverse_transform(predicted_prices)
         print(f"\nResult: {predicted_prices}")
 
         # Plot The Test Predicitons
