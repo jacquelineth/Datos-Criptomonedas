5ef3c7714d5a7fc4141ffa80c6e7812e9f2887dd
jacquelineth <jacquelineth@gmail.com>
2021-07-12 23:40:33 +0000

    Corrected import for 3.7

--- datareader.py
+++ datareader.py
@@ -1,9 +1,9 @@
 import pip
-pip.main(['install', 'scikit'])
+pip.main(['install', 'scikit-learn'])
 pip.main(['install', 'tensorflow'])
-pip.main(['install', 'itertools'])
+
 pip.main(['install', 'pandas'])
-pip.main(['install', 'matplotlib.pyplot'])
+pip.main(['install', 'matplotlib'])
 pip.main(['install', 'mplfinance'])
 pip.main(['install', 'seaborn'])
 pip.main(['install', 'numpy'])
